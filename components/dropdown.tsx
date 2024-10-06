a168aa2e4f5f07d923b79906e7b1b54dc6115363
STEVIE <stephenoke2007@gmail.com>
2024-10-06 22:29:19 +0100

    shapely for life

--- components/dropdown.tsx
+++ components/dropdown.tsx
@@ -6,7 +6,7 @@ export function DropDownMenu() {
             <select name="" id="" className="w-[20rem] p-3 border rounded-lg ml-[20%] text-white mt-[-3 0px] mb-4">
               {locations.map(function(each){
                 return (
-                    <option value={each}>{each}</option>
+                    <option key={each} value={each}>{each}</option>
                 )
               })}
             </select>
